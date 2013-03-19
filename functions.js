525a305b4a44c5aeb9e468d268244e8d2e729581
unknown <microsoftAX@PC-HUYMAC.vodc.lan>
2013-03-19 17:24:07 +0700

    Fonts

---
+++ functions.js
@@ -0,0 +1,4 @@
+Cufon.replace('span, tr.video-item', { fontFamily: 'GothamBook' });
+Cufon.replace('h3', { fontFamily: 'GothamBold' });
+Cufon.replace('.total-video, h1, h2, p', { fontFamily: 'GothamLight' });
+Cufon.replace('.status p', { fontFamily: 'GothamMedium' });
